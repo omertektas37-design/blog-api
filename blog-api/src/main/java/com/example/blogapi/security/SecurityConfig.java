249eb6913e642fbace8d2d4a1795d67dd89dad0c
omertektas37-design <omertektas.37@gmail.com>
2026-04-08 12:27:58 +0300

    Update SecurityConfig.java

--- blog-api/src/main/java/com/example/blogapi/security/SecurityConfig.java
+++ blog-api/src/main/java/com/example/blogapi/security/SecurityConfig.java
@@ -6,16 +6,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Configuration
 public class SecurityConfig {
-
     @Bean
     public org.springframework.security.web.SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
         http.csrf().disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/auth/**", "/swagger-ui/**", "/v3/api-docs/**")
-                .permitAll()
-                .anyRequest().authenticated();
-
+            .authorizeHttpRequests()
+            .requestMatchers("/auth/**","/swagger-ui/**","/v3/api-docs/**").permitAll()
+            .anyRequest().authenticated();
         return http.build();
     }
 }
