c7ee6bf3b57c16fdf4206c0955df0bc489675520
omertektas37-design <omertektas.37@gmail.com>
2026-04-08 12:23:08 +0300

    Update and rename PostRepository.java to Post.java

--- blog-api/src/main/java/com/example/blogapi/model/PostRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.blogapi.repository;
-
-import com.example.blogapi.model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {}
