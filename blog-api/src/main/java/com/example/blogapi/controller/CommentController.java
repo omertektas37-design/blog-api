54eb1b54d9219553eeae501512dd1711c6007253
omertektas37-design <omertektas.37@gmail.com>
2026-04-08 12:27:17 +0300

    Update CommentController.java

--- blog-api/src/main/java/com/example/blogapi/controller/CommentController.java
+++ blog-api/src/main/java/com/example/blogapi/controller/CommentController.java
@@ -4,23 +4,17 @@ import com.example.blogapi.model.Comment;
 import com.example.blogapi.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
 @RequestMapping("/comments")
 public class CommentController {
-
     @Autowired
     private CommentRepository commentRepository;
 
     @PostMapping
-    public Comment add(@RequestBody Comment comment) {
-        return commentRepository.save(comment);
-    }
+    public Comment add(@RequestBody Comment comment) { return commentRepository.save(comment); }
 
     @GetMapping
-    public List<Comment> getAll() {
-        return commentRepository.findAll();
-    }
+    public List<Comment> getAll() { return commentRepository.findAll(); }
 }
