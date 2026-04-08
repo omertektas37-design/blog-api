2ceca3baf91ab832f7bb5d5562bef9e94561ceb1
omertektas37-design <omertektas.37@gmail.com>
2026-04-08 12:26:37 +0300

    Refactor PostController for improved readability

--- blog-api/src/main/java/com/example/blogapi/controller/PostController.java
+++ blog-api/src/main/java/com/example/blogapi/controller/PostController.java
@@ -4,25 +4,19 @@ import com.example.blogapi.model.Post;
 import com.example.blogapi.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
 @RequestMapping("/posts")
 public class PostController {
-
     @Autowired
     private PostRepository postRepository;
 
     @PostMapping
-    public Post create(@RequestBody Post post) {
-        return postRepository.save(post);
-    }
+    public Post create(@RequestBody Post post) { return postRepository.save(post); }
 
     @GetMapping
-    public List<Post> getAll() {
-        return postRepository.findAll();
-    }
+    public List<Post> getAll() { return postRepository.findAll(); }
 
     @PutMapping("/{id}")
     public Post update(@PathVariable Long id, @RequestBody Post post) {
@@ -31,7 +25,5 @@ public class PostController {
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
-        postRepository.deleteById(id);
-    }
+    public void delete(@PathVariable Long id) { postRepository.deleteById(id); }
 }
