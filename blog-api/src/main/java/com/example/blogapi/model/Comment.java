a66c2089992b1edf828cc32843e25fbc3b4c5313
omertektas37-design <omertektas.37@gmail.com>
2026-04-08 12:23:55 +0300

    Update and rename CommentRepository.java to Comment.java

---
+++ blog-api/src/main/java/com/example/blogapi/model/Comment.java
@@ -0,0 +1,22 @@
+package com.example.blogapi.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String text;
+
+    @ManyToOne
+    private Post post;
+
+    // getter / setter
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getText() { return text; }
+    public void setText(String text) { this.text = text; }
+    public Post getPost() { return post; }
+    public void setPost(Post post) { this.post = post; }
+}
--- blog-api/src/main/java/com/example/blogapi/model/CommentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.blogapi.repository;
-
-import com.example.blogapi.model.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {}
